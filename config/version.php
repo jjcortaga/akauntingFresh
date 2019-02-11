9e982c685d9092a0cca09181db610d44cdcce6c6
jepz12345 <jjcortaga@gmail.com>
2019-02-11 18:43:05 +0800

    first commit

---
+++ config/version.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'name'      =>  'Akaunting',
+
+    'code'      =>  'Reconciliation',
+
+    'major'     =>  '1',
+
+    'minor'     =>  '3',
+
+    'patch'     =>  '11',
+
+    'build'     =>  '',
+
+    'status'    =>  'Stable',
+
+    'date'      =>  '04-February-2019',
+
+    'time'      =>  '19:00',
+
+    'zone'      =>  'GMT +3',
+
+];
